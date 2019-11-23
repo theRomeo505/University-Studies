b4d404e2e64d49383d391969edc4565a30d78bb8
theRomeo505 <41187645+theRomeo505@users.noreply.github.com>
2019-11-23 09:52:39 +0200

    laba5 done

--- labka6/labka6.cpp
+++ labka6/labka6.cpp
@@ -3,47 +3,93 @@
 
 #include <iostream>
 #include <vector>
+#include <cstdlib>
 using namespace std;
 int main()
 {
 	char* m = new char[256];
 	cin.getline(m,256);
-	vector <int> equation;
-	int y = 0,l=0;
+	vector <double> equation;
+	double y = 0; int  l = 0; bool flazok=0;
 	while (m[l] > 0)
 	{
 		l++;
 	}
-
+	//cout << l;
 	for (int i = 0; i < l; i++) {
-		if (m[i] == ' ' && m[i-1]>=0)
+		if (m[i] == ' ' )
 		{
-			equation.push_back(y);
-			y = 0;
-		}
-		
-		
+			if (isdigit(m[i - 1]))
+			{
+				if (flazok)
+				equation.push_back(-1*y);
+				else
+				equation.push_back(y);
+				y = 0;
+				flazok = 0;
+			}
+		}else		
 		if (m[i] == '+') {
-			equation.push_back(-1); 
-		}if (m[i] == '-') {
-			equation.push_back(-2); 
-		}if (m[i] == '*') {
-			equation.push_back(-3); 
-		}if (m[i] == '/') {
-			equation.push_back(-4); 
-		}if (m[i] == '^') {
-			equation.push_back(-5); 
+			if (!isdigit(m[i+1]))
+			equation.push_back(-0.1); 
+		}else if (m[i] == '-') {
+			if (isdigit(m[i + 1]))
+				flazok = 1;
+			else
+			equation.push_back(-0.2); 
+		}else if (m[i] == '*') {
+			equation.push_back(-0.3); 
+		}else if (m[i] == '/') {
+			equation.push_back(-0.4); 
+		}else if (m[i] == '^') {
+			equation.push_back(-0.5); 
 		}
 		else {
 			y *= 10;
-			y += m[i] - 48;
+			y += (double)(m[i] - 48);
 		}
-	}
+	}//last
+	//cout << equation.size();
+//	while (equation.size() > 1) {
 	for (int i = 0; i < equation.size(); i++) {
-			cout << m[i];
-	}
-	for (int i = l - 1; i--; i >= 0) {
+		cout << equation[i] << ' ';
 
-	}
+	}cout << endl;
+		for (int i = equation.size(); i--; i >= 2) {
+			//cout << equation.size() << endl;
+			//cout << "i" << i << endl;
+			if ((equation[i]>-1 && equation[i] < 0) && (equation[i - 1] >= 0 || equation[i-1]<=-1) && (equation[i - 2] >= 0 || equation[i - 2] <= -1) ){
+				
+				
+				if (equation[i] == -0.1) {
+					equation[i - 2] += equation[i - 1];
+					equation.erase(equation.begin() + i - 1); equation.erase(equation.begin() + i - 1);
+					
+				}else if (equation[i] == -0.2) {
+					equation[i - 2] -= equation[i - 1];
+					equation.erase(equation.begin() + i - 1); equation.erase(equation.begin() + i - 1);
+				}else if (equation[i] == -0.3) {
+					equation[i - 2] *= equation[i - 1];
+					equation.erase(equation.begin() + i - 1); equation.erase(equation.begin() + i - 1);
+				} else 	if (equation[i] == -0.4) {
+					equation[i - 2] = (int)equation[i-2]/equation[i - 1];
+					equation.erase(equation.begin() + i - 1); equation.erase(equation.begin() + i - 1);
+				} else 	if (equation[i] == -0.5) {
+					equation[i - 2] = pow(equation[i - 2], equation[i - 1]);
+					equation.erase(equation.begin() + i - 1); equation.erase(equation.begin() + i - 1);
+				}
+				
+				
+				i = equation.size();
+
+			}
+			
+		}
+		if (equation.size() == 1)
+		{
+			cout << equation[0];
+			return 0;
+		}
+	//}
 }
 
