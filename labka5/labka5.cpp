014b42be4040a1c2fbe6be1a594fc56091172558
theRomeo505 <41187645+theRomeo505@users.noreply.github.com>
2019-12-01 19:06:42 +0200

    щжшожщшо

--- labka5/labka5.cpp
+++ labka5/labka5.cpp
@@ -1,12 +1,14 @@
 ﻿// labka5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
-
+#include <vector>
 #include <iostream>
 #include <bitset>
 #include <fstream>
 using namespace std;
 void labka5task1() {
-	ifstream f("laba.txt");
+	//_getch(c);
+	
+	ifstream f("laba.txt"); //char c; f.get(c);
 	char* s = new char[1000]; int i = 0;
 	while (!f.eof()) {
 		f.read(s, 256);
@@ -45,19 +47,21 @@ void labka5task2() {
 	}
 }//////////////////////////////
 void md5(char* p, int n) {
-	bitset<32> s[64] = { 7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22, 5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20, 4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23, 6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21 };
-	bitset<32> k[64] = { 0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee , 0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501 ,0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be ,0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821 ,0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa ,0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8 , 0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed ,0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c , 0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70 , 0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05,0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665, 0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039,0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1, 0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391 };
-	bitset<32> a0 = 0x67452301;   //A
-	bitset<32> b0 = 0xefcdab89;   //B
-	bitset<32> c0 = 0x98badcfe;   //C
-	bitset<32> d0 = 0x10325476;   //D
+	int s[64] = { 7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22, 5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20, 4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23, 6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21 };
+	unsigned int k[64] = { 0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee , 0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501 ,0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be ,0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821 ,0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa ,0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8 , 0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed ,0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c , 0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70 , 0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05,0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665, 0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039,0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1, 0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391 };
+	unsigned int a0 = 0x67452301;   //A
+	unsigned int b0 = 0xefcdab89;   //B
+	unsigned int c0 = 0x98badcfe;   //C
+	unsigned int d0 = 0x10325476;   //D
 	string ss; p += 1;
 	//cout <<p<<"k"<<sizeof(p);
-	int a = 6;
-	int nn = 9;//bool k;
-	bitset<512> * u = new bitset<512>[ceil((double)n / 64) +1];
+	//int a = 6;
+	//int nn = 9;//bool k;
+	unsigned int ** u = new unsigned int* [ceil((double)n / 64) +1];
+	for (int i = 0; i < ceil((double)n / 64) + 1; i++)
+		u[i] = new unsigned int[8];
 	int j,jui= ceil((double)n / 64);
-	for ( j = 0; j < ceil((double)n / 64-1) ; j++) {
+	/*for ( j = 0; j < ceil((double)n / 64-1) ; j++) {
 		//const size_t N = 4;  // bound on string length
 		
 		for (int i = 0; i < 64; i++) {
@@ -78,8 +82,23 @@ void md5(char* p, int n) {
 			u[j][512 - ii - 1] = k;
 		}
 		
+	}*/
+	for (j = 0; j < ceil((double)n / 64 -1); j++) {
+		for (int i = 64 * j+3; i < 64 * j + 64; i+=4) {
+			int y = 0;
+			for (int g = i+4; g > i ; g--) {
+				y *= 256;
+				y += p[g];
+			}
+			u[j][(i - 64 * j) / 4]=y;
+			cout << u[j][(i - 64 * j) / 4] << ' ';
+		}
+		cout << endl;
 	}
-	int i;
+	system("pause");
+
+	////
+	/*int i;
 	for (i = 0; i < 64; i++) {
 		if (p[i] < 0)
 			break;
@@ -114,14 +133,64 @@ void md5(char* p, int n) {
 			ll++;
 			
 		}
+	}*/
+	////
+	int **uu = new int* [jui];
+	int r;
+	for (int i = 0; i < jui; i++) {
+		uu[i] = new int[16];
+		for (int j = 0; j < 16; j++) {
+			r = 0x0;
+			for (int g = 32 * (j); g < 32*(j +1); g++) {
+				r *= 2;
+				r += u[i][g];
+			} ///// divide into groups 4 bit each zen skleit
+			switch (r) {
+			case 1:
+				uu[i][j] = 0x1;
+				break;case 2:
+				uu[i][j] = 0x2;
+				break;case 3:
+				uu[i][j] = 0x3;
+				break;case 4:
+				uu[i][j] = 0x4;
+				break;case 5:
+				uu[i][j] = 0x5;
+				break;case 6:
+				uu[i][j] = 0x6;
+				break;case 7:
+				uu[i][j] = 0x7;
+				break;case 8:
+				uu[i][j] = 0x8;
+				break;case 9:
+				uu[i][j] = 0x9;
+				break;case 10:
+				uu[i][j] = 0xa;
+				break;case 11:
+				uu[i][j] = 0xb;
+				break;case 12:
+				uu[i][j] = 0xc;
+				break;case 13:
+				uu[i][j] = 0xd;
+				break;case 14:
+				uu[i][j] = 0xe;
+				break;case 15:
+				uu[i][j] = 0xf;
+				break;
+			}
+
+
+			//
+		}
 	}
+
 	cout << u[j] << endl;
-	bitset<32> A, B, C, D,f, g;
+	int A, B, C, D,f, g;
 	for (int j = 0; j < jui; j++) {
-		bitset<32> m[16];
+		int m[16];
 		for (int i = 0; i < 16; i++) {
 			for (int ii = 32 * i; ii < 32 * i + 32; ii++) {
-				m[i][ii % 32] = u[j][ii];
+			//	m[i][ii % 32] = u[j][ii];
 			}
 		}
 		A = a0;
@@ -162,6 +231,10 @@ int main()
 {
 	char s[11] = " bibb cock";
 	//int b = 13;
+	//int y = 0x0;
+	//cout << (y + 0xa)<<' ';
+	//unsigned int i = pow(2, 32);
+	//cout << i << endl;
 	md5(s, 10);
 }
 
