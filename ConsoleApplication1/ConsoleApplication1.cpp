251a000b2fab03fc200ce5480f1f16ac935cdab1
theRomeo505 <41187645+theRomeo505@users.noreply.github.com>
2020-04-05 12:55:24 +0300

    Update ConsoleApplication1.cpp

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -11,77 +11,72 @@ struct pol {
 	pol* left;
 	pol* right;
 };
+typedef pol *node;
 int prior(char s) {
 	int lp = 0;
-	//cout << "bat ebal";
-	if (s== '+' || s == '-')
+	if (s == '+' || s == '-')
 		lp = 1;
 	else if (s == '*' || s == '/' || s == '%')
 		lp = 2;
 	else if (s == '^')
 		lp = 3;
+	else
+		lp = 60;
 	return lp;
 }
-vector <pol> cr (string s)
-{
-	vector <pol> m; int lp; int k = 0; pol p;
-	for (int i = 0; i < s.size(); i++) {
-		lp = prior(s[i]);
-		k = 0;
-		if (lp == 0) {
-			while (isdigit(s[i]) && s[i]>=0) {
-				k *= 10;
-				k += s[i] - 48;
-				i++;
-			}
-			p.isnum = 0;
-			p.nnum = k;
-			m.push_back(p); i--;
-		}
-		else {
-		//	cout << "mat ebal";
-			p.isnum = lp;
-			if (s[i] == '+')
-				p.nnum = 1;if (s[i] == '-')
-				p.nnum = 2;if (s[i] == '%')
-				p.nnum = 3;if (s[i] == '*')
-				p.nnum = 4;if (s[i] == '/')
-				p.nnum = 5;if (s[i] == '^')
-				p.nnum = 6;
-			m.push_back(p);
-		}
-	}
-	for (int i = 0; i < m.size(); i++) {
-		cout << m[i].isnum << ' ' << m[i].nnum << endl;
-	}
-	return m;
-}/*
-pol createP(string s,int f,int l) {
-	
-	pol *p=new pol;// koef bukva step bukva step
+
+node createP(string s,int f,int l) {
+	node tree=new pol;
 	
 	int k = 0,lp=0;
-	
-	if (f == l) {
-		p->isnum = lp;
-		if (!lp) {
-			p->nnum = k;
+	for (int i = f; i <= l; i++) {
+		if (!isdigit(s[i])) {
+			lp = 1;
+			break;
 		}
-		p->left = NULL;
-		p->right = NULL;
+		k *= 10;
+		k += s[i]-48;
+	}
+	if (!lp) { /// from f to l number
+		tree->isnum = 1;
+		tree->nnum = k;
+		tree->left = NULL;
+		tree->right = NULL;
+		
+		return tree;
 	}
 	int min = 60, prt; k = 0;
-	for (int i = f; i < l; i++) {
+	for (int i = f; i <= l; i++) {
 		prt = prior(s[i]);
 		if (prt < min) {
 			min = prt;
 			k = i;
 		}
 	}
-	p.
-	return *p;
+	tree->isnum = 0;
+	tree->nnum = s[k];
+	tree->left = createP(s,f,k-1);
+	tree->right = createP(s, k + 1, l);
+	return tree;
 }
-*/
+int tr(node tree) {
+	if (tree->left==NULL) {
+		
+		return tree->nnum;
+	}
+	int num1 = tr(tree->left);
+	int num2 = tr(tree->right);
+	switch ((char)tree->nnum) {
+	case '+': return num1 + num2;
+	case '-': return num1 - num2;
+	case '*': return num1 * num2;
+	case '/': return num1 / num2;
+	case '^': return pow(num1,num2);
+
+	}
+	tr(tree->left);
+	tr(tree->right);
+}
 int main()
 {
 	string s;
@@ -91,35 +86,24 @@ int main()
 		if (isalpha(s[i])) {
 			cout << "cin" << s[i] << endl;
 			cin >> y;
-//			bukv[s[i] - 'a']=y;
-			for (int j = i; j < s.size(); j++) {
-				if (s[j] == s[i]) {
-					double f = y;
-					while (f > 10) {
-						f /= 10;
-						//cout << f << endl;
-					}
-					s[j] = (int)f +48;
-					f = f - (int)f;
-					//cout << endl;
-					f *= 10;
-					while (f != 0) {
-						
-						//system("Pause");
-						s.insert(s.begin() + j + 1, (int)f + 48); //cout << f << endl;
-						if ((int)(f*10)%10==0) {
-							f = 0;
-						}
-						else {
-							f = f - (int)f; 
-							f *= 10;
-						}
-						//cout << f << endl;
-					}
-				}
+			vector<int> yy;
+			while (y > 0) {
+				yy.push_back(y % 10);
+				y /= 10;
+			}
+					s[i] = yy[yy.size() - 1]+48;
+					yy.pop_back();
+					
+					while (yy.size()>0) {
+						s.insert(s.begin() + i + 1, yy[yy.size()-1]+48); 
+						yy.pop_back(); i++;
+			
 			}
 		}
 	}
 	cout << s<<endl;
-	vector <pol> p = cr(s);
+	node p = createP(s, 0, s.size() - 1);
+	system("Pause");
+	
+	cout<<tr(p);
 }
