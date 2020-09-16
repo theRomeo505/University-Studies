cd0a2d13b651cddbf3fefcc6a1abac86c2e9f61d
dev148 <dev148@devsteam.mobi>
2020-09-16 09:49:11 +0300

    im back

---
+++ 2kurs1sem1lab-BD/2kurs1sem1lab-BD.cpp
@@ -0,0 +1,285 @@
+﻿// 2kurs1sem1lab-BD.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Customer {
+	int key;
+	char name[20];
+	int rating;
+	bool exists;
+};
+struct Project {
+	int key;
+	char name[20];
+	int price;
+	int custkey;
+	bool exists;
+};
+
+void createCust() {
+	FILE *f=fopen("customers", "ab+"); /// ab+
+	Customer c;
+	cout << "enter name:\n";
+	cin >> c.name;
+	cout << "enter rating";
+	cin >> c.rating;
+	c.exists = true;
+	fseek(f, 0, 2);
+	//cout << ftell(f)<<' '<<sizeof(Customer)<<endl;
+	c.key = ftell(f) / sizeof(Customer);
+	fseek(f, SEEK_END, 1);
+	fwrite(&c,sizeof(Customer),1,f);
+	fclose(f);
+}
+void createProj() {
+	FILE* f = fopen("projects", "ab+"); /// ab+
+	Project c;
+	cout << "enter name:\n";
+	cin >> c.name;
+	cout << "enter price";
+	cin >> c.price;
+	cout << "enter customer key";
+	cin >> c.custkey;
+	c.exists = true;
+	fseek(f, 0, 2);
+	//cout << ftell(f)<<' '<<sizeof(Customer)<<endl;
+	c.key = ftell(f) / sizeof(Project);
+	fseek(f, SEEK_END,1);
+	fwrite(&c, sizeof(Project), 1, f);
+	fclose(f);
+}
+
+
+void deleteCust(int id) {
+	FILE* f=fopen("customers","rb");
+	vector<Customer> m;
+	Customer p;
+	while (fread(&p, sizeof(Customer), 1, f) != NULL) {
+		m.push_back(p);
+	}
+	m[id].exists = 0;
+	fclose(f);
+
+	f = fopen("customers", "wb");
+	for (int i = 0; i < m.size(); i++)
+		fwrite(&m[i], sizeof(Customer), 1, f);
+	fclose(f);
+	m.clear();
+	/////
+	f = fopen("projects", "rb");
+	vector<Project> proj;
+	Project pr;
+	while (fread(&pr, sizeof(Project), 1, f) != NULL) {
+		if (pr.custkey == id)
+			pr.exists = 0;
+		proj.push_back(pr);
+	}
+	fclose(f);
+	f = fopen("projects", "wb");
+	for (int i = 0; i < proj.size(); i++)
+		fwrite(&proj[i], sizeof(Project), 1, f);
+	fclose(f);
+	proj.clear();
+	
+}
+void deleteProj(int id) {
+	FILE* f = fopen("projects", "rb");
+	vector<Project> proj;
+	Project pr;
+	while (fread(&pr, sizeof(Project), 1, f)) {
+		proj.push_back(pr);
+	}
+	fclose(f);
+	f = fopen("projects", "wb");
+	for (int i = 0; i < proj.size(); i++) {
+		if (i == id)
+			proj[i].exists = false;
+		fwrite(&proj[i], sizeof(Project), 1, f);
+	}
+	proj.clear();
+	fclose(f);
+}
+
+void changeCust(int id) {
+	FILE* f = fopen("customers", "rb");
+	vector<Customer> custs;
+	Customer cust;
+	while (fread(&cust, sizeof(Customer), 1, f))
+		custs.push_back(cust);
+	fclose(f);
+	f = fopen("customers", "wb");
+	for (int i = 0; i < custs.size(); i++) {
+		if (i == id) {
+			char g[20];
+			cout << "enter name (or 0 to keep)";
+			cin >> g;
+			if (g != "0")
+				strcpy(custs[i].name ,g);
+			cout << "enter rating(or 0 tio keep)";
+			int gg;
+			cin >> gg;
+			if (g != "0")
+				custs[i].rating = gg;
+		}
+		fwrite(&custs[i], sizeof(Customer), 1, f);
+	}
+	fclose(f);
+}
+void changeProj(int id) {
+	FILE* f = fopen("projects", "rb");
+	vector<Project> projs;
+	Project proj;
+	while (fread(&proj, sizeof(Project), 1, f))
+		projs.push_back(proj);
+	fclose(f);
+	f = fopen("projects", "wb");
+	for (int i = 0; i < projs.size(); i++) {
+		if (i == id) {
+			char g[20];
+			cout << "enter name (or 0 to keep)"<<endl;
+			cin >> g;
+			if (g != "0")
+				strcpy(projs[i].name ,g);
+			cout << "enter price(or 0 to keep)" << endl;
+			int gg;
+			cin >> gg;
+			if (g != "0")
+				projs[i].price = gg;
+			cout << "enter customer (or -1 to keep)" << endl;
+			cin >> gg;
+			if (gg != -1)
+				projs[i].custkey = gg;
+		}
+		fwrite(&projs[i], sizeof(Project), 1, f);
+	}
+	fclose(f);
+}
+
+void infoCust(int id) {
+	FILE* f = fopen("customers", "rb");
+	fseek(f, sizeof(Customer)*id, 0);
+	
+	Customer cust;
+	fread(&cust, sizeof(Customer), 1, f);
+	cout << cust.name<< ' '<<cust.rating<<endl;
+	cout << "projects:" << endl;
+	fclose(f);
+
+	f = fopen("projects", "rb");
+	Project pr; int count = 0;
+	while (fread(&pr, sizeof(Project), 1, f) != NULL) {
+		if (pr.custkey == id && pr.exists)
+		{
+			cout << pr.name << ' ' << pr.price << endl; count++;
+		}
+	}
+	cout << count << " projects"<<endl;
+	//cout << "ku";
+
+	fclose(f);
+	//cout << "ku";
+}
+void infoProj(int id) {
+	FILE* f=fopen("projects","rb");
+	Project pr;
+	fseek(f, sizeof(Project) * id, 1);
+	fread(&pr, sizeof(Project), 1, f);
+	cout << pr.name << ' ' << pr.price << endl;
+	fclose(f);
+	f = fopen("customers", "rb");
+	fseek(f, sizeof(Customer) * pr.custkey, 1);
+	Customer cust;
+	fread(&cust, sizeof(Customer), 1, f);
+	cout << cust.name << ' ' << cust.rating << endl;
+	fclose(f);
+}
+
+void coutAll() {
+	FILE* f = fopen("customers", "rb");
+	Customer r;
+	while (fread(&r, sizeof(Customer), 1, f) != NULL) {
+		cout << r.name << ' ' << r.key << ' ' << r.exists << endl;
+		
+	}
+	cout << endl;
+	fclose(f);
+	f = fopen("projects", "rb");
+	Project p;
+	while (fread(&p, sizeof(Project), 1, f) != NULL) {
+		cout << p.name << ' ' << p.key<< ' '<< p.custkey << ' ' << p.exists << endl;
+
+	}
+
+	fclose(f);
+}
+
+void count()
+{
+	FILE* f = fopen("projects", "rb");
+	fseek(f, 0, 2);
+	int count = ftell(f) / sizeof(Project);
+	fclose(f);
+	f = fopen("customers", "rb");
+	fseek(f, 0, 2);
+	cout << count << "projects" << endl;
+	count = ftell(f) / sizeof(Customer);
+	cout << count << "customers";
+	fclose(f);
+	
+}
+int main()
+{
+	cout << "1-create cust" << endl;
+	cout << "2-create proj" << endl;
+	cout << "3-delete cust" << endl;
+	cout << "4-delete proj" << endl;
+	cout << "5-change cust" << endl;
+	cout << "6-change proj" << endl;
+	cout << "7-info cust" << endl;
+	cout << "8-info proj" << endl;
+	cout << "9-show all" << endl;
+	int a;int id;
+	while (true) {
+		cin >> a;
+		switch (a) {
+		case 1:
+			createCust();
+			break;
+		case 2:
+			createProj(); break;
+		case 3:
+			
+			cout << "enter id" << endl;
+			cin >> id;
+			deleteCust(id); break;
+		case 4:
+			cout << "enter id" << endl;
+			cin >> id;
+			deleteProj(id); break;
+		case 5:
+			cout << "enter id" << endl;
+			cin >> id;
+			changeCust(id); break;
+		case 6:
+			cout << "enter id" << endl;
+			cin >> id;
+			changeProj(id); break;
+		case 7:
+			cout << "enter id" << endl;
+			cin >> id;
+			infoCust(id); break;
+		case 8:
+			cout << "enter id" << endl;
+			cin >> id;
+			infoProj(id); break;
+		case 9:
+			coutAll(); break;
+		}
+	}
+	return 0;
+}
+
+
---
+++ 2kurs1sem1lab_algor/2kurs1sem1lab_algor.cpp
@@ -0,0 +1,127 @@
+﻿// 2kurs1sem1lab_algor.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <fstream>
+#include <time.h>
+#include <vector>
+using namespace std;
+void quickSort(vector<int>& array, int low, int high)
+{
+	int i = low;
+	int j = high;
+	int pivot = array[(i + j) / 2];
+	int temp;
+
+	while (i <= j)
+	{
+		while (array[i] < pivot)
+			i++;
+		while (array[j] > pivot)
+			j--;
+		if (i <= j)
+		{
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+			i++;
+			j--;
+		}
+	}
+	if (j > low)
+		quickSort(array, low, j);
+	if (i < high)
+		quickSort(array, i, high);
+}
+
+int main()
+{
+	srand(time(NULL));
+	int n, max, files;
+	cin >> n >> max >> files;
+	int peaces = (n) / (max)+((n%max==0)?0:1);
+	if ((peaces-1)%(files-2)!=0)
+	{
+		peaces += files - 1 - peaces % (files - 2);
+	}
+	// 0 1 equal 
+	int* m = new int[n];
+	for (int i = 0; i < n; i++)
+		m[i] = rand() % 200;
+	// in data
+	int inpeace = n / peaces;
+	ofstream f;
+	f.open("file0");
+	f.close();
+	f.open("file1");
+	vector <int> mm;
+	int i = 0;
+	for (i = 0; i < inpeace;i++) {
+		mm.push_back(m[i]);
+	}
+	quickSort(mm, 0, inpeace - 1);
+	for (i = 0; i < inpeace; i++) {
+		f << mm[i];
+	}
+	
+	f.close();
+	int g = 3;
+	f.open("file2");
+	for (i = 1; i < peaces; i++) {
+		mm.clear();
+		for (int j = 0; j < inpeace; j++) {
+			mm.push_back(m[inpeace * i + j]);
+		}
+		if (i == peaces - 1)
+		{
+			int k = inpeace * peaces;
+			while (k < n) {
+				mm.push_back(m[k]);
+				k++;
+			}
+		}
+		quickSort(mm, 0, mm.size() - 1);
+		for (int j = 0; j < mm.size(); j++) {
+			f << mm[j];
+			cout << mm[j]<<' ';
+		}
+		cout << endl;
+		if (i % ((peaces - 1) / (files - 2)) == 0) {
+			cout << endl;
+			f.close();
+			f.open("file" + (g+'0'));
+			g++;
+		}
+	}
+	f.close();
+	///////////////////
+	vector<fstream> file;
+	ofstream f;
+
+	for (i = 0; i < files; i++)
+	{
+		f.open("file"+(i+'0'));
+	
+		file.push_back(f);
+		f.close;
+	}
+	while (!file[2].eof()) {
+		int f;
+		file[2] >> f;
+		cout << f << ' ';
+	}
+	for (i = 0; i < (peaces - 1) % (files - 2); i++) {
+
+	}
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
